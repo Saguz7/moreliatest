9bfbedba875094b198c558783d7afdef6630bece
Cesar Santiago <cesar.santiago@sweettech.io>
2023-11-06 20:26:51 -0600

    Primer commit en el nuevo repositorio

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,10 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import Head from 'next/head' // Importa el componente Head
+import Head from 'next/head'
 import './globals.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import fetch from 'node-fetch';
+ 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
@@ -19,7 +20,6 @@ export default function RootLayout({
   return (
     <html lang="es">
       <Head>
-        {/* Agrega la hoja de estilos en la sección <head> */}
         <link href="https://ui.michoacan.gob.mx/static/css/main.edcd66f5.css" rel="stylesheet" />
       </Head>
       <body className={inter.className}>{children}</body>
